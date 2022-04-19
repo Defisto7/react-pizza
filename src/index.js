5efb53ca392d38506ab67e8198f42913a4f45bc1
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-04-19 15:17:22 +0300

    master
    add PizzaBlock component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 //import ReactDOM from 'react-dom'
 
 import './scss/app.scss';
@@ -9,7 +10,12 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-    <App />
+  <BrowserRouter>
+  <Routes>
+    <Route path="*" element={<App />} />
+  </Routes>
+  </BrowserRouter>
+  
   //</React.StrictMode>
 );
 
