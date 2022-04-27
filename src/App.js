13d3fcaadbc33e5855c83b8f733b98182faf7828
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-04-27 09:56:08 +0300

    master
    add Redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react';
 import { Route, Routes} from 'react-router-dom'
 import { useDispatch } from 'react-redux'
@@ -6,15 +5,13 @@ import { useDispatch } from 'react-redux'
 
 import { Header } from './components';
 import { Home , Cart } from './pages';
-import { setPizzas } from './redux/actions/pizzas'
+import { fetchPizzas } from './redux/actions/pizzas'
 
 function App() {
   const dispatch = useDispatch();
   
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    console.log(dispatch(fetchPizzas()))
   });
 
   return (
